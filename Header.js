4820a16440d2ca811a2ab67e1276062b10f50e89
kartikey0029sharma <43802998+kartikey0029sharma@users.noreply.github.com>
2020-12-23 14:35:46 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Header.css';
+
+function Header(){
+    return(
+        <div>
+            <h1 className="header"><Link className="headerLink" to="/">Breaking Bad</Link></h1>
+        </div>
+    );
+}
+
+export default Header;
